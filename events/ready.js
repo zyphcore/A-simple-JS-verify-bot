b6b0896f34490b8ecec16c5ec95716307d2aaf5c
zyphcore <111462734+zyphcore@users.noreply.github.com>
2023-06-06 13:07:52 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,11 @@ const client = require("../index");
 
 client.on("ready", () =>
     console.log(`${client.user.tag} is up and ready to go!`)
-);
+    client.user.setPresence({
+        status: "idle",  //You can show online, idle....
+        game: {
+            name: "zyph#0001",  //The message shown
+            type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
+ });
